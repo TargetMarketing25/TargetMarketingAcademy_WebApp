56bd122c10d56d7b0bce26afe7433af25132402a
Tarek Nabil <tarekidm2011@gmail.com>
2025-10-28 01:54:06 +0300

    update design or text

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+body {
+  @apply bg-dark text-white font-sans overflow-x-hidden;
+}
